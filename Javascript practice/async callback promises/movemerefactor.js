8adf50ba10d7fcaab34139a67e07436d5a66b00d
Richa Basnet <prabasnet1@gmail.com>
2025-01-31 14:26:37 -0600

    refactoring promises

--- Javascript practice/async callback promises/movemerefactor.js
+++ Javascript practice/async callback promises/movemerefactor.js
@@ -7,7 +7,7 @@ const moveX = (element, amount, delay) => {
       const elRight = element.getBoundingClientRect().right;
       const currLeft = element.getBoundingClientRect().left;
       if (elRight + amount > bodyBoundary) {
-        reject();
+        reject({bodyBoundary, elRight, amount});
       } else {
         element.style.transform = `translateX(${currLeft + amount}px)`;
         resolve(() => moveX(element, amount * 2, delay)); // Chain another move
@@ -17,7 +17,13 @@ const moveX = (element, amount, delay) => {
 };
 
 
-moveX(btn, 300, 1000).then(() => {
-    console.log("DONE MOVING!");
-    
-});
+moveX(btn, 300, 1000)
+.then(() => moveX(btn, 300,1000))
+.then(() => moveX(btn, 300,1000))
+.then(() => moveX(btn, 300,1000))
+.then(() => moveX(btn, 300,1000))
+.then(() => moveX(btn, 300,1000))
+.catch(({bodyBoundary,amount, elRight}) => {
+    console.log(`Body is ${bodyBoundary}px wide`);
+    console.log(`Element is at ${elRight}px, ${amount}px ;is too large`);
+})
